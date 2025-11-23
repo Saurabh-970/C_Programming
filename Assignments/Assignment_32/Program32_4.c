c8d438c1cae9fbdee278e805811c9f3c60150f6b
Saurabh Bhonsle <141349409+Saurabh-970@users.noreply.github.com>
2025-11-23 16:20:31 +0530

    Add files via upload

---
+++ Assignments/Assignment_32/Program32_4.c
@@ -0,0 +1,91 @@
+///////////////////////////////////////////////////////////////////////////////////////////
+//
+//  Required header files
+//
+///////////////////////////////////////////////////////////////////////////////////////////
+
+#include<stdio.h>
+
+///////////////////////////////////////////////////////////////////////////////////////////
+//
+//  Function name : Pattern
+//  Description   : Accept number of rows and columns and display the pattern
+//  Input         : Integer
+//  Output        : pattern
+//  Author        : Saurabh Ravindra Bhonsle
+//  Date          : 20 /11/2025
+//
+///////////////////////////////////////////////////////////////////////////////////////////
+
+
+void Pattern(int iRow, int iCol)
+{
+  int i = 0;
+  int j = 0;
+
+  if( iRow != iCol)
+  {
+    printf("Invalid input");
+    printf("Row number and column number should be same");
+    return;
+  }
+
+  for(i = 1; i <= iRow; i++)
+  { 
+      for(j = 1; j <= iCol; j++)
+      {
+        if((j == 1) || (j == iCol) || (i == 1) || (i == iRow))
+        {
+            printf("*\t");
+        }
+        else if (i + j < iRow + 1)
+        {
+            printf("#\t");
+        }
+        else if ( i + j > iRow +1)
+        {
+            printf("$\t");
+        }
+        else
+        {
+            printf("*\t");
+        }
+      }
+      printf("\n");
+  }
+}
+
+///////////////////////////////////////////////////////////////////////////////////////////
+//
+//  Entry point function of the application
+//
+///////////////////////////////////////////////////////////////////////////////////////////
+
+int main()
+{
+    int iValue1 = 0;
+    int iValue2 = 0;
+
+    printf("Enter number of rows and columns :");
+    scanf("%d %d",&iValue1, &iValue2);
+
+    Pattern(iValue1,iValue2);
+
+    return 0;
+
+}//End of main
+
+/////////////////////////////////////////////////////////////////////////////////////////
+//
+//  Testcases successfully handled by the application
+//
+//    Enter number of rows and columns   : rows :4, columns: 4
+//
+//    Output :  *       *       *       *       *       *
+//              *       #       #       #       *       *
+//              *       #       #       *       $       *
+//              *       #       *       $       $       *
+//              *       *       $       $       $       *
+//              *       *       *       *       *       *
+//                          
+///////////////////////////////////////////////////////////////////////////////////////////
